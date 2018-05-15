bb3529780f9c9d63c21be09debd1ea65252d8f95
usagi <earlobe3302@gmail.com>
2018-05-15 20:50:49 +0900

    Mainどこ

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,25 +1,13 @@
-
-import org.chasen.mecab.Tagger
 import org.chasen.mecab.MeCab
-
-import scala.collection.mutable
 
 object Main {
   def main(args: Array[String]): Unit = {
 
-    //-Djava.library.path=/usr/lib/jni
-    System.loadLibrary("MeCab")
-    val tagger = new Tagger()
-    val str = "太郎は二郎にこの本を渡した。"
-    tagger.parse(str)
-    var node = tagger.parseToNode(str)
-    val doc = Document(mutable.MutableList.empty[Fragment], 1)
-    while(node != null){
-      println(node.getSurface + "\t" + node.getFeature)
+    val l = List("http://icserv01.forest.eis.ynu.ac.jp/?p=50","https://docs.oracle.com/javase/jp/7/api/java/util/HashSet.html")
 
-      node = node.getNext
-    }
-    println(doc.getText())
+    println(DataInputer(l).getDidntCalcCurationMap.documents.last.getText())
+
+    //println(DataInputer("http://icserv01.forest.eis.ynu.ac.jp/?p=50").getInput)
   }
 
 
