e1a3cb89a1743d65ab47911925b041bff886c26c
usagi <earlobe3302@gmail.com>
2018-05-18 21:55:17 +0900

    dassai

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,9 +1,9 @@
 package controllers
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
+import us.feliscat.text.StringOption
 import util.DataInputer
-
-import us.feliscat.text.analyzer.mor.mecab.UnidicMecab
+import us.feliscat.text.analyzer.mor.mecab.{IpadicMecab, UnidicMecab}
 
 class HomeController  @Inject()(cc: ControllerComponents) (implicit assetsFinder: AssetsFinder)
   extends AbstractController(cc) {
@@ -14,6 +14,8 @@ class HomeController  @Inject()(cc: ControllerComponents) (implicit assetsFinder
 //
 //    cuMap.genLink()
 //    Ok(cuMap.documents.last.getText())
-    Ok("kfjsojfjsdofjsiodf")
+
+    println(IpadicMecab.analyze(StringOption("Linux（我が家の環境ではUbuntu12.04）でファイルをサクっと検索したい場合に何を使うか\n長らく自分はfindを使っていたが、どうもlocateの方が高速だし手軽らしいという話を聞きつけた。")).next())
+    Ok( "dd")
   }
 }
--- app/util/DataInputer.scala
+++ app/util/DataInputer.scala
@@ -1,37 +1,53 @@
 package util
 
+import java.nio.charset.StandardCharsets
+
 import data.{CurationMap, Document, Fragment, Morpheme}
-import org.chasen.mecab.{Node, Tagger}
 
 import scala.collection.mutable
+import us.feliscat.text.analyzer._
+import us.feliscat.text.analyzer.mor.mecab.IpadicMecab
+
+import java.nio.charset.{CodingErrorAction, StandardCharsets}
+
+import us.feliscat.text.{StringNone, StringOption}
+import us.feliscat.util.LibrariesConfig
+import us.feliscat.util.process._
 
 case class DataInputer(sourceList : List[String]){
 
   val docSet = mutable.HashSet.empty[Document]
 
+
+
+
+
+
+
   sourceList.foreach {
     source =>
-      //要変更
-      System.loadLibrary("MeCab")
-      val tagger = new Tagger()
-      val str: String = GetterFromWeb(source).getInput
-      tagger.parse(str)
-      var node: Node = tagger.parseToNode(str)
       val doc = Document(mutable.MutableList.empty[Fragment], sourceList.indexOf(source))
-      val queue: mutable.MutableList[Morpheme] = mutable.MutableList.empty[Morpheme]
-      while (Option[Node](node).isDefined) {
-        //println(node.getSurface + "\t" + node.getFeature)
-        val m = Morpheme(node.getSurface, node.getFeature)
-        if (m.getPartsOfSpeech() != "BOS/EOS") {
-          queue += m
-          if (queue.last.getSubPartsOfSpeech() == "句点") {
-            doc.fragList += Fragment(queue.toList)
-           // println(doc.fragList.last)
-            queue.clear()
-          }
-        }
-        node = node.getNext
-      }
+      //要変更
+//      System.loadLibrary("MeCab")
+//      val tagger = new Tagger()
+//      val str: String = GetterFromWeb(source).getInput
+//      tagger.parse(str)
+//      var node: Node = tagger.parseToNode(str)
+//      val doc = Document(mutable.MutableList.empty[Fragment], sourceList.indexOf(source))
+//      val queue: mutable.MutableList[Morpheme] = mutable.MutableList.empty[Morpheme]
+//      while (Option[Node](node).isDefined) {
+//        //println(node.getSurface + "\t" + node.getFeature)
+//        val m = Morpheme(node.getSurface, node.getFeature)
+//        if (m.getPartsOfSpeech() != "BOS/EOS") {
+//          queue += m
+//          if (queue.last.getSubPartsOfSpeech() == "句点") {
+//            doc.fragList += Fragment(queue.toList)
+//           // println(doc.fragList.last)
+//            queue.clear()
+//          }
+//        }
+//        node = node.getNext
+//      }
       //println(doc.getText())
       docSet += doc
   }
